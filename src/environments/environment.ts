205c50c094a9373b0beb643b022138de659db919
jubbajub <sefer.derya@gmx.de>
2022-01-14 14:58:25 +0100

    Data-Service an Modal created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAtb1EwutomDE1Z5iopcJDyBObKLa9Bbrg",
+    authDomain: "sd-projects-6161.firebaseapp.com",
+    projectId: "sd-projects-6161",
+    storageBucket: "sd-projects-6161.appspot.com",
+    messagingSenderId: "490452213204",
+    appId: "1:490452213204:web:1c4a2846477443c2be0a41",
+    measurementId: "G-STGXP4YLEB"
+  }
 };
 
 /*
